131e44f5e192028ceb7deb9fd4001d7bc6f7950a
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-18 03:17:22 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,10 +4,4 @@ import Link from './Link.js';
 import Click from './Click.js';
 import Setting from './Setting.js';
 
-User.hasMany(Link, { foreignKey: 'userId' });
-Link.belongsTo(User, { foreignKey: 'userId' });
-
-Link.hasMany(Click, { foreignKey: 'linkId' });
-Click.belongsTo(Link, { foreignKey: 'linkId' });
-
 export { sequelize, User, Link, Click, Setting };
