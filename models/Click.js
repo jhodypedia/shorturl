6dc03dcc3da8f85724b6e987d5b866694e32d1c5
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-18 03:16:11 +0700

    Update Click.js

--- models/Click.js
+++ models/Click.js
@@ -1,5 +1,6 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/db.js';
+import Link from './Link.js';
 
 const Click = sequelize.define('clicks', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -9,5 +10,12 @@ const Click = sequelize.define('clicks', {
   country: { type: DataTypes.STRING(3), allowNull: true },
   device: { type: DataTypes.STRING, allowNull: true },
   referer: { type: DataTypes.TEXT, allowNull: true }
+}, {
+  timestamps: true // ✅ createdAt & updatedAt otomatis
 });
+
+// Relasi
+Click.belongsTo(Link, { foreignKey: 'linkId' });
+Link.hasMany(Click, { foreignKey: 'linkId' });
+
 export default Click;
