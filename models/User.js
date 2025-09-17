e1ea3e98790a30d7dc1be71968f117fe66bc61ed
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-18 03:16:58 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,9 +4,12 @@ import sequelize from '../config/db.js';
 const User = sequelize.define('users', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM('admin','user'), defaultValue: 'user' },
-  status: { type: DataTypes.ENUM('active','suspended'), defaultValue: 'active' }
+  role: { type: DataTypes.STRING, defaultValue: 'user' }, // admin/user
+  status: { type: DataTypes.STRING, defaultValue: 'active' } // active/suspended
+}, {
+  timestamps: true // ✅ createdAt & updatedAt otomatis
 });
+
 export default User;
