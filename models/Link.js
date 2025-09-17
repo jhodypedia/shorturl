1830206703c6165e5206a734a96db0f2f68a06bd
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-18 03:16:37 +0700

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -1,12 +1,19 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/db.js';
+import User from './User.js';
 
 const Link = sequelize.define('links', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  code: { type: DataTypes.STRING(12), unique: true },
+  code: { type: DataTypes.STRING, allowNull: false, unique: true },
   target: { type: DataTypes.TEXT, allowNull: false },
   title: { type: DataTypes.STRING, allowNull: true },
-  userId: { type: DataTypes.INTEGER, allowNull: false },
   enabled: { type: DataTypes.BOOLEAN, defaultValue: true }
+}, {
+  timestamps: true // ✅ createdAt & updatedAt otomatis
 });
+
+// Relasi
+Link.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Link, { foreignKey: 'userId' });
+
 export default Link;
